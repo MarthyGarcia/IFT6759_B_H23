29ab54ff9d1daedd641a44c5aa60cc027dfb91bf
williamglazer <willcavanagh@hotmail.com>
2023-02-27 18:49:33 -0500

    added cookiecutter template with S3 -> GDrive

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of time series forecasting state of the art for cryptocurrency applications',
+    author='williamGlazer',
+    license='MIT',
+)
